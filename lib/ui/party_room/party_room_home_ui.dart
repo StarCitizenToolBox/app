01ad2fb9653616f3a37b33ad35e9aec44a827432
xkeyC <3334969096@qq.com>
2024-01-30 21:45:51 +0800

    增加问卷链接

--- lib/ui/party_room/party_room_home_ui.dart
+++ lib/ui/party_room/party_room_home_ui.dart
@@ -8,6 +8,7 @@ import 'package:flutter_tilt/flutter_tilt.dart';
 import 'package:starcitizen_doctor/base/ui.dart';
 import 'package:starcitizen_doctor/generated/grpc/party_room_server/index.pb.dart';
 import 'package:starcitizen_doctor/widgets/cache_image.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 import 'party_room_home_ui_model.dart';
 
@@ -23,11 +24,32 @@ class PartyRoomHomeUI extends BaseUI<PartyRoomHomeUIModel> {
   @override
   Widget build(BuildContext context) {
     // final model = ref.watch(provider);
-    return const Center(
+    return Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Text("敬请期待！"),
+          const Text(
+            "联机大厅，敬请期待 ！",
+            style: TextStyle(fontSize: 20),
+          ),
+          const SizedBox(height: 12),
+          GestureDetector(
+            onTap: () {
+              launchUrlString("https://wj.qq.com/s2/14112124/f4c8/");
+            },
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text("诚邀您参与 "),
+                Text(
+                  "问卷调查。",
+                  style: TextStyle(
+                    color: Colors.blue,
+                  ),
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
