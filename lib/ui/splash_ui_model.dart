02101db99d24c8cb4ae067a736a323ef3ded9ca9
xkeyC <3334969096@qq.com>
2024-02-22 20:26:13 +0800

    BinaryModule support

--- lib/ui/splash_ui_model.dart
+++ lib/ui/splash_ui_model.dart
@@ -1,5 +1,6 @@
 import 'package:starcitizen_doctor/api/analytics.dart';
 import 'package:starcitizen_doctor/base/ui_model.dart';
+import 'package:starcitizen_doctor/common/conf/binary_conf.dart';
 import 'package:starcitizen_doctor/common/conf/url_conf.dart';
 import 'package:starcitizen_doctor/ui/index_ui.dart';
 import 'package:starcitizen_doctor/ui/index_ui_model.dart';
@@ -27,6 +28,7 @@ class SplashUIModel extends BaseUIModel {
     await AppConf.checkUpdate();
     step = 2;
     notifyListeners();
+    await handleError(() => BinaryModuleConf.extractModel());
     Future.delayed(const Duration(milliseconds: 300));
     Navigator.pushAndRemoveUntil(
         context!,
