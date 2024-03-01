c6b69c4e08c692de334e77a79e0e8bca9c038eb7
xkeyC <3334969096@qq.com>
2024-03-01 21:06:47 +0800

    MES: 2.10.7.0 42

--- lib/common/conf/app_conf.dart
+++ lib/common/conf/app_conf.dart
@@ -18,9 +18,9 @@ import 'package:uuid/uuid.dart';
 import 'package:window_manager/window_manager.dart';
 
 class AppConf {
-  static const String appVersion = "2.10.6 Beta";
-  static const int appVersionCode = 41;
-  static const String appVersionDate = "2024-02-26";
+  static const String appVersion = "2.10.7 Beta";
+  static const int appVersionCode = 42;
+  static const String appVersionDate = "2024-03-01";
 
   static const gameChannels = ["LIVE", "PTU", "EPTU"];
 
