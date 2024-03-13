35ce760b8d5340414851db4032f21198283c6707
xkeyC <3334969096@qq.com>
2024-03-13 22:42:56 +0800

    release: 2.10.8 Beta 43

--- lib/common/conf/const_conf.dart
+++ lib/common/conf/const_conf.dart
@@ -1,6 +1,6 @@
 class ConstConf {
-  static const String appVersion = "2.10.7 Beta";
-  static const int appVersionCode = 42;
+  static const String appVersion = "2.10.8 Beta";
+  static const int appVersionCode = 43;
   static const String appVersionDate = "2024-03-01";
   static const gameChannels = ["LIVE", "PTU", "EPTU"];
   static const isMSE =
