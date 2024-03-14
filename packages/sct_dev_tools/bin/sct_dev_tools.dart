7882159139ce4f592cfac6d31ec79948bcb205b2
xkeyC <3334969096@qq.com>
2024-03-15 00:03:04 +0800

    fix: rename args

--- packages/sct_dev_tools/bin/sct_dev_tools.dart
+++ packages/sct_dev_tools/bin/sct_dev_tools.dart
@@ -2,9 +2,9 @@ import 'auto_l10n.dart';
 
 void main(List<String> args) {
   switch (args.elementAtOrNull(0)) {
-    case "gen":
+    case "l10n_gen":
       return AutoL10nTools().genL10nFiles();
-    case "replace":
+    case "l10n_replace":
       return AutoL10nTools().replaceL10nFiles();
     default:
       throw Exception("cmd not found");
