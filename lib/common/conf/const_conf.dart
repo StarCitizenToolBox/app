4d2c0c20d5b5b7e084d7b831456b3ef13b2b07a3
xkeyC <3334969096@qq.com>
2024-03-26 20:05:01 +0800

    Release: V2.10.9

--- lib/common/conf/const_conf.dart
+++ lib/common/conf/const_conf.dart
@@ -1,7 +1,7 @@
 class ConstConf {
-  static const String appVersion = "2.10.8 Beta";
-  static const int appVersionCode = 43;
-  static const String appVersionDate = "2024-03-01";
+  static const String appVersion = "2.10.9 Beta";
+  static const int appVersionCode = 44;
+  static const String appVersionDate = "2024-03-26";
   static const gameChannels = ["LIVE", "PTU", "EPTU"];
   static const isMSE =
       String.fromEnvironment("MSE", defaultValue: "false") == "true";
