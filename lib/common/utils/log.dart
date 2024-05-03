dd14a34285d895de2e57e37575ac65089ebb00df
xkeyC <3334969096@qq.com>
2024-05-03 13:12:11 +0800

    feat: delete logs when inDays > 7

--- lib/common/utils/log.dart
+++ lib/common/utils/log.dart
@@ -19,8 +19,22 @@ void dPrint(src) async {
   });
 }
 
-void setDPrintFile(File file) {
-  _logFile = file;
+Future<void> initDPrintFile(String applicationSupportDir) async {
+  final now = DateTime.now();
+  final logFile =
+      File("$applicationSupportDir\\logs\\${now.millisecondsSinceEpoch}.log");
+  await logFile.create(recursive: true);
+  _logFile = logFile;
+  final logsDir = Directory("$applicationSupportDir\\logs");
+  await for (final files in logsDir.list()) {
+    if (files is File) {
+      final stat = await files.stat();
+      if (stat.type == FileSystemEntityType.file &&
+          now.difference(await files.lastModified()).inDays > 7) {
+        await files.delete();
+      }
+    }
+  }
 }
 
 File? getDPrintFile() {
