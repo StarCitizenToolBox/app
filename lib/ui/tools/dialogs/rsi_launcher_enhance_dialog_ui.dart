b530742634cf788224d573c927611eac313ea4ca
xkeyC <3334969096@qq.com>
2024-06-04 20:29:55 +0800

    feat: RsiLauncherEnhance add zh_TW Support

--- lib/ui/tools/dialogs/rsi_launcher_enhance_dialog_ui.dart
+++ lib/ui/tools/dialogs/rsi_launcher_enhance_dialog_ui.dart
@@ -38,6 +38,7 @@ class RsiLauncherEnhanceDialogUI extends HookConsumerWidget {
   static const supportLocalizationMap = {
     "en": NoL10n.langEn,
     "zh_CN": NoL10n.langZHS,
+    "zh_TW": NoL10n.langZHT,
     "fr": NoL10n.langFR,
   };
 
