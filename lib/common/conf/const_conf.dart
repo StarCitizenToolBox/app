f283f4db438d1cc4a51bd1e507c46128168408aa
xkeyC <3334969096@qq.com>
2024-09-11 20:50:01 +0800

    feat: hotfix channel support

--- lib/common/conf/const_conf.dart
+++ lib/common/conf/const_conf.dart
@@ -2,7 +2,7 @@ class ConstConf {
   static const String appVersion = "2.12.0";
   static const int appVersionCode = 54;
   static const String appVersionDate = "2024-6-28";
-  static const gameChannels = ["LIVE", "PTU", "EPTU", "TECH-PREVIEW"];
+  static const gameChannels = ["LIVE", "PTU", "EPTU", "TECH-PREVIEW", "HOTFIX"];
   static const isMSE =
       String.fromEnvironment("MSE", defaultValue: "false") == "true";
 }
