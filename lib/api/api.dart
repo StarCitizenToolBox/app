ac99c4b608df24d2cbb01832c7bf500d44d8f957
xkeyC <3334969096@qq.com>
2025-11-08 15:21:55 +0800

    fix: Api

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -12,17 +12,14 @@ import 'package:starcitizen_doctor/data/sc_localization_data.dart';
 
 class Api {
   static Future<AppVersionData> getAppVersion() async {
-    return AppVersionData.fromJson(
-        await getRepoJson("sc_doctor", "version.json"));
+    return AppVersionData.fromJson(await getRepoJson("sc_doctor", "version.json"));
   }
 
   static Future<AppPlacardData> getAppPlacard() async {
-    return AppPlacardData.fromJson(
-        await getRepoJson("sc_doctor", "placard.json"));
+    return AppPlacardData.fromJson(await getRepoJson("sc_doctor", "placard.json"));
   }
 
-  static Future<List<CountdownFestivalItemData>>
-      getFestivalCountdownList() async {
+  static Future<List<CountdownFestivalItemData>> getFestivalCountdownList() async {
     List<CountdownFestivalItemData> l = [];
     final r = json.decode(await getRepoData("sc_doctor", "countdown.json"));
     if (r is List) {
@@ -30,19 +27,15 @@ class Api {
         l.add(CountdownFestivalItemData.fromJson(element));
       }
     }
-    l.sort((a, b) => (a.time ?? 0) - (b.time ?? 0));
     return l;
   }
 
-  static Future<Map<String, dynamic>> getAppReleaseDataByVersionName(
-      String version) async {
-    final r = await RSHttp.getText(
-        "${URLConf.gitlabApiPath}repos/SCToolBox/Release/releases/tags/$version");
+  static Future<Map<String, dynamic>> getAppReleaseDataByVersionName(String version) async {
+    final r = await RSHttp.getText("${URLConf.gitlabApiPath}repos/SCToolBox/Release/releases/tags/$version");
     return json.decode(r);
   }
 
-  static Future<List<ScLocalizationData>> getScLocalizationData(
-      String lang) async {
+  static Future<List<ScLocalizationData>> getScLocalizationData(String lang) async {
     final data = json.decode(await getRepoData("localizations", "$lang.json"));
     List<ScLocalizationData> l = [];
     if (data is List) {
@@ -80,28 +73,24 @@ class Api {
   }
 
   static Future<List> getScServerStatus() async {
-    final r = await RSHttp.getText(
-        "https://status.robertsspaceindustries.com/index.json");
+    final r = await RSHttp.getText("https://status.robertsspaceindustries.com/index.json");
     final map = json.decode(r);
     return map["systems"];
   }
 
-  static Future<Map<String, dynamic>> getRepoJson(
-      String dir, String name) async {
+  static Future<Map<String, dynamic>> getRepoJson(String dir, String name) async {
     final data = await getRepoData(dir, name);
     return json.decode(data);
   }
 
   static Future<String> getRepoData(String dir, String name) async {
-    final r = await RSHttp.getText("${URLConf.apiRepoPath}/$dir/$name",
-        withCustomDns: await isUseInternalDNS());
+    final r = await RSHttp.getText("${URLConf.apiRepoPath}/$dir/$name", withCustomDns: await isUseInternalDNS());
     return r;
   }
 
   static Future<bool> isUseInternalDNS() async {
     final userBox = await Hive.openBox("app_conf");
-    final isUseInternalDNS =
-        userBox.get("isUseInternalDNS", defaultValue: false);
+    final isUseInternalDNS = userBox.get("isUseInternalDNS", defaultValue: false);
     return isUseInternalDNS;
   }
 }
