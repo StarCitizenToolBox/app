e48adb3cac0edcfab446e422c58f3f444dbbd675
xkeyC <3334969096@qq.com>
2026-06-09 22:30:47 +0800

    fix: version bump

--- lib/common/conf/conf.dart
+++ lib/common/conf/conf.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 
 class ConstConf {
-  static const String appVersion = "3.2.2";
-  static const int appVersionCode = 83;
+  static const String appVersion = "3.2.3";
+  static const int appVersionCode = 84;
   static const String appVersionDate = "2026-06-09";
   static const _gameChannels = ["LIVE", "4.0_PREVIEW", "PTU", "EPTU", "TECH-PREVIEW", "HOTFIX"];
   static const isMSE = String.fromEnvironment("MSE", defaultValue: "false") == "true";
